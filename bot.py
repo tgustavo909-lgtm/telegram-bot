1905ee2604a604719577941e123b7d4f45a286b2
tgustavo909-lgtm <tgustavo909@gmail.com>
2025-11-16 23:17:43 -0300

    fdghtrh

--- bot.py
+++ bot.py
@@ -1,23 +1,2 @@
-import telebot
-import time
 
-TOKEN = 8505121686:AAFfTD5pMiU7SiVH4joZa6k_vbWtH46w67o
 
-bot = telebot.TeleBot(TOKEN)
-
-# Mensagem inicial
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Olá! Envie a prova de pagamento que eu libero o acesso manualmente.")
-
-# Comando para você liberar vídeos manualmente
-@bot.message_handler(commands=['liberar'])
-def liberar_acesso(message):
-    bot.reply_to(message, "Acesso liberado! (isso é só um comando de exemplo)")
-
-# Bot rodando
-while True:
-    try:
-        bot.polling(none_stop=True)
-    except Exception:
-        time.sleep(5)
